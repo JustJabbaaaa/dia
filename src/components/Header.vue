c0346f554c0f20cc482c24a6e4d57a4d5b0be515
JustJabbaaaa <adriankotowski217@gmail.com>
2025-05-15 18:34:20 +0200

    heloł

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
   <Section anchor="Header" grid="2">
     <div class="header__left">
       <div class="header__description">
-        <h1 class="header__description--title">Lorem, ipsum.</h1>
+        <h1 class="header__description--title">Eldorado benc.</h1>
         <p class="header__description--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias cum delectus ab quo labore deleniti doloremque illo repellendus praesentium porro maiores incidunt vel, animi quia voluptatum et suscipit voluptate! Quia veniam illo maiores modi quaerat facere quis odio voluptas corrupti?</p>
       </div>
       <!-- <div class="header__buttons">
