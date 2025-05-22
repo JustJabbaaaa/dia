8e65a670b318cb6ae319d3ec08941123dcb922e4
JustJabbaaaa <adriankotowski217@gmail.com>
2025-05-22 17:21:10 +0200

    forgot link

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,7 @@
       <div class="header__description">
         <h1 class="header__description--title">Eldorado benc.</h1>
         <p class="header__description--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias cum delectus ab quo labore deleniti doloremque illo repellendus praesentium porro maiores incidunt vel, animi quia voluptatum et suscipit voluptate! Quia veniam illo maiores modi quaerat facere quis odio voluptas corrupti?</p>
-        <NuxtLink to="" class="btn btn-primary" target="_blank">Start now!</NuxtLink>
+        <NuxtLink to="https://app.orionaccountsmanager.com/" class="btn btn-primary" target="_blank">Start now!</NuxtLink>
       </div>
     </div>
     <div class="header__right">
